811bac3c8b8fe58e29a74595475bb04ce08027b2
Natalya <natalyasafrevich@gmail.com>
2023-01-18 22:39:47 +0400

    add similar books

--- app/src/components/similarBooks/SimilarBooks.jsx
+++ app/src/components/similarBooks/SimilarBooks.jsx
@@ -1,9 +1,14 @@
-import './SimilarBooks.jsx'
+import "./SimilarBooks.jsx";
 
-export default function SimilarBooks (){
-    return(
-        <>
-        'similars book'
-        </>
-    )
+export default function SimilarBooks() {
+	return (
+		<div className="similar-book">
+			<div className="similar-book__container">
+                <p className="similar-book__name">
+                    Similar Book
+                </p>
+                
+            </div>
+		</div>
+	);
 }
