bae7618085d0745b9325cf95c2f09d3fe45e5c3a
Natalya <natalyasafarevich@gmail.com>
2023-03-14 11:41:33 +0400

    ThemeProvider was added

--- app/src/components/preview/Preview.jsx
+++ app/src/components/preview/Preview.jsx
@@ -1,20 +1,36 @@
 import { Link } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import MenuBookOutlinedIcon from "@mui/icons-material/MenuBookOutlined";
 import "./Preview.scss";
 
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: "#fff",
+		},
+	},
+});
 export function Preview() {
 	return (
-		<div className="preview">
-			<div className="preview__container">
-				<div className="preview__text">
-					<p className="preview__title">
-						Read <span>Books </span> Online
-					</p>
-					<p className="preview__desc">
-						Let you read books online without leaving website
-					</p>
+		<ThemeProvider theme={theme}>
+			<div className="preview">
+				<div className="preview__container">
+					<div className="preview__text">
+						<p className="preview__title">
+							Read <span>Books </span> Online
+						</p>
+						<p className="preview__desc">
+							Let you read books online without leaving website
+						</p>
+					</div>
+					<div className="preview__image"></div>
 				</div>
-				<div className="preview__image"></div>
+				<div className="preview__info">
+					<div className="preview__item">
+						<MenuBookOutlinedIcon color="primary" />
+					</div>
+				</div>
 			</div>
-		</div>
+		</ThemeProvider>
 	);
 }
