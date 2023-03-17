775ab16e32adbdb6e49500bf8b59f1fe8beab1d6
Natalya <natalyasafarevich@gmail.com>
2023-03-17 16:22:34 +0400

    notification styles were changed

--- app/src/components/notification/Notification.jsx
+++ app/src/components/notification/Notification.jsx
@@ -4,9 +4,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { setNotificationHidden } from "../../store/notification/actions";
 import "./Notification.scss";
 
-export default function Notification({ book, action }) {
+export default function Notification() {
 	const text = useSelector((state) => state.note);
-	const dispatch = useDispatch();
 
 	useEffect(() => {
 		if (!text.hidden) {
@@ -25,6 +24,7 @@ export default function Notification({ book, action }) {
 	return (
 		<div className="notification ">
 			<div className="notification__container">
+				<div className="notification__icon"> </div>
 				<p className="notification__desc">
 					Book: <i>"{text.text}"</i>
 					<span> is added in favorite</span>
